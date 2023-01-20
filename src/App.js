b83796a4c8675672123d3bb8cbbbdf8d8ea35b68
Buck <ryan42buck@gmail.com>
2023-01-19 22:17:42 -0500

    added new folder to main page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import TeamSelect from "./components/TeamSelect";
+import PlayerInput from "./components/PlayerInput";
 
 function App() {
   return (
     <div className="App">
+      <PlayerInput />
       <TeamSelect />
     </div>
   );

cf4619543ecdfb0fcb7b37df92bfdce95a396c76
Buck <ryan42buck@gmail.com>
2023-01-19 21:17:16 -0500

    possibly completed randomizer - still need to test

--- src/utils/randomizer.js
+++ src/utils/randomizer.js
@@ -1,8 +1,28 @@
-export const randomizer = function ([players], teams) {
+const shuffle = function (array) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+  return array;
+};
+
+export const randomizer = function (players, teams) {
   const maxTeamSize = players.length / teams;
   //shuffle players then assign to teams in order.
-
-  for (let i = 0; i < players.length; i++) {
-    const teammate = players[i];
-  }
+  shuffle(players);
+  const completedTeams = () =>
+    players.length > maxTeamSize
+      ? [
+          players.slice(0, maxTeamSize),
+          ...completedTeams(players.slice(maxTeamSize), maxTeamSize),
+        ]
+      : [players];
 };
